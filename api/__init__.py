6c05d9db930b059886ec3d4177801d9d1a8b2c93
yego5 <shachz.github@gmail.com>
2024-01-09 18:59:44 -0800

    api AirBnb

--- api/__init__.py
+++ api/__init__.py
@@ -1,43 +1,18 @@
 #!/usr/bin/python3
 '''
-Createw Flask app; and register the blueprint app_views to Flask instance app.
+Creates a Blueprint instance with `url_prefix` set to `/api/v1`.
 '''
 
-from os import getenv
-from flask import Flask, jsonify
-from flask_cors import CORS
-from models import storage
-from api.v1.views import app_views
 
-app = Flask(__name__)
+from flask import Blueprint
 
-# enable CORS and allow for origins:
-CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0'}})
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
-
-
-@app.teardown_appcontext
-def teardown_engine(exception):
-    '''
-    Removes the current SQLAlchemy Session object after each request.
-    '''
-    storage.close()
-
-
-# Error handlers for expected app behavior:
-@app.errorhandler(404)
-def not_found(error):
-    '''
-    Return errmsg `Not Found`.
-    '''
-    response = {'error': 'Not found'}
-    return jsonify(response), 404
-
-
-if __name__ == '__main__':
-    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
-    PORT = int(getenv('HBNB_API_PORT', 5000))
-    app.run(host=HOST, port=PORT, threaded=True)
-#@shachz
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *
